6c157ec2b7842bf236fb7481956fc5a40fd891eb
Jacob Friis Saxberg <jacob@saxberg.dk>
2014-03-11 23:26:38 -0400

    closes #15

--- models/user.js
+++ models/user.js
@@ -5,21 +5,29 @@ var schema = new mongoose.Schema({
   username: String
 });
 
-module.exports.login = function (req, res, next) {
+module.exports.login = function (req, res) {
   jsonBody(req, res, function (err, body) {
     if (err) {
       res.statusCode = 500;
       throw err;
       return res.end('HTTP error');
     }
-    var user = mongoose.model('users', schema);
+    var User = mongoose.model('users', schema);
     var cond = {
       username: body.username
     }
-    user.findOne(cond, function (err, user) {
+    User.findOne(cond, function (err, user) {
       if (err) throw err;
-      req.session.user = user;
-      res.json(user);
+      if (user) {
+        req.session.user = user;
+        res.json(user);
+      } else {
+        User.create(cond, function (err, user) {
+          if (err) throw err;
+          req.session.user = user;
+          res.json(user);
+        });
+      }
     });
   });
 }
