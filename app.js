7f98064d3a3179689c34be134d5211dd7bcc9cd8
Jacob Friis Saxberg <jacob@saxberg.dk>
2014-03-12 17:57:29 -0400

    wip

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
 var express = require('express');
 var routes = require('./routes');
 var mongoose = require('mongoose');
-// var MongoStore = require('connect-mongo')(express);
 var expressSession = require('express-session');
 var cookieParser = require('cookie-parser');
 var compress = require('compression');
 var favicon = require('static-favicon');
 
 var dbconf = {
-  mongoose: {
-    url: 'mongodb://asapp:loveBoat@troup.mongohq.com:10041/asapp',
-    auto_reconnect: true
-  },
-  sessions: {
-    url: 'mongodb://asapp:loveBoat@troup.mongohq.com:10041/asapp',
-    auto_reconnect: true
-  },
+  url: 'mongodb://asapp:loveBoat@troup.mongohq.com:10041/asapp',
   secret: 'go with the waves'
 };
 
-mongoose.connect(dbconf.mongoose.url);
+mongoose.connect(dbconf.url, {
+  auto_reconnect: true
+});
+mongoose.connection.on('connected', function () {
+  console.log('MongoDB connected');
+});
+mongoose.connection.on('error', function (err) {
+  console.error(err);
+});
 
 var app = express();
 module.exports = app;
 
 app.set('port', process.env.PORT || 3000);
-app.use(compress());
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(express.static(__dirname + '/public'));
 
 // if (app.get('env') == 'development') {
 //   var logger = require('morgan');
 //   app.use(logger());
 // }
+
+app.use(compress());
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(express.static(__dirname + '/public'));
 
 app.use(cookieParser());
 app.use(expressSession({
@@ -44,3 +45,4 @@ routes.define(router);
 app.use(router);
 
 app.listen(app.get('port'));
+console.log('Express listening on port %d', app.get('port'));
