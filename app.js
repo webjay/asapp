8250f242c11cb6d28c6fe0043634ba48b0fa1cd8
Jacob Friis Saxberg <jacob@saxberg.dk>
2014-07-24 13:12:30 -0700

    Opbeat

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ var cookieParser = require('cookie-parser');
 var compress = require('compression');
 var favicon = require('serve-favicon');
 
+var opbeat = require('opbeat'); 
+var opbeat_client = opbeat.createClient({ 
+  organization_id: '49efe7c522234909a3dce43d20dc76ff',
+  app_id: '3706f3bfea',
+  secret_token: '93233cf8840e10a2988cc4af51e9aa885bcc01ca'
+});
+
 var dbconf = {
   url: process.env.MONGOURL || 'mongodb://mongo:bongo@kahana.mongohq.com:10064/asapp_dev',
   secret: 'go with the waves'
@@ -48,6 +55,14 @@ var router = express.Router();
 routes.define(router, io);
 app.use(router);
 
-// app.listen(app.get('port'));
+app.use(function (err, req, res, next) {
+  res.json(500, { error: err });
+  console.error(err);
+  opbeat_client.captureError(err, function (opbeatErr, url) {
+    if (opbeatErr) return console.error(opbeatErr);
+    console.log('The error can be found on:', url);
+  });
+});
+
 server.listen(app.get('port'));
 console.log('Express listening on port %d', app.get('port'));
